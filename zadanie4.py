b07c91840ae6038f8df420014136bfe937ea63d8
whnt0001 <97937508+whnt0001@users.noreply.github.com>
2024-01-24 16:28:45 +0300

    Update zadanie4.py

--- zadanie4.py
+++ zadanie4.py
@@ -1,13 +1,10 @@
 n = int(input())
 i = 1
-j = 1
-cnt = 1
-num = 1
+num = 1 
 while i <= n:
-    i  += 1
-    while j <= cnt:
-        print(num, end = ' ')
-        num += 1
+    i += 1
+    for j in range(i):
+        print(num, end = " ")
+        num += 1 
         j += 1
     print()
-    cnt +=1
